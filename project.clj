2fae5b33268424b383c59aa76aaf703f87cf7afc
Andrey Antukh <niwi@niwi.be>
2014-11-06 18:19:23 +0100

    Set first release version: 0.1.0-alpha

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject suricatta "0.1.0-SNAPSHOT"
+(defproject suricatta "0.1.0-alpha"
   :description "High level sql toolkit for clojure (backed by jooq library)"
   :url "https://github.com/niwibe/suricatta"
 
