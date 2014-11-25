5af7b1245175ce84422b9464182f2cd1a55b2a31
Andrey Antukh <niwi@niwi.be>
2014-11-26 00:33:38 +0100

    Update dependencies versions.
    jOOQ: from 3.4.4 to 3.5.0
    clojure.jdbc: 0.3.0 to 0.3.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject suricatta "0.1.0-alpha"
+(defproject suricatta "0.1.1-alpha"
   :description "High level sql toolkit for clojure (backed by jooq library)"
   :url "https://github.com/niwibe/suricatta"
 
@@ -10,7 +10,7 @@
                                   [cheshire "5.3.1"]]
                    :global-vars {*warn-on-reflection* true}}}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.jooq/jooq "3.4.4"]
-                 [clojure.jdbc "0.3.0"]
+                 [org.jooq/jooq "3.5.0"]
+                 [clojure.jdbc "0.3.2"]
                  [cats "0.2.0" :exclusions [org.clojure/clojure]]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]])
