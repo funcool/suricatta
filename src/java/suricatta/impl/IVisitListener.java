1d83f8186d965f53e7851f553261d273eebbc04a
Andrey Antukh <niwi@niwi.be>
2014-12-05 01:31:56 +0100

    Remove now unused VisitListener wrapper.

--- src/java/suricatta/impl/IVisitListener.java
+++
@@ -1,8 +0,0 @@
-package suricatta.impl;
-
-import org.jooq.VisitContext;
-
-public interface IVisitListener {
-  public Object start(VisitContext context);
-  public Object end(VisitContext context);
-}
