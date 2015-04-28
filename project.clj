34fe513359ca99cfd8ab58399a6590bf975a04a4
Andrey Antukh <niwi@niwi.be>
2015-04-28 17:35:18 +0200

    Update dependencies.

--- project.clj
+++ project.clj
@@ -4,17 +4,16 @@
   :license {:name "BSD (2-Clause)"
             :url "http://opensource.org/licenses/BSD-2-Clause"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.jooq/jooq "3.5.3"]
-                 [cats "0.3.2" :exclusions [org.clojure/clojure]]
-                 [clojure.jdbc "0.4.0"]
+                 [org.jooq/jooq "3.6.0"]
+                 [cats "0.4.0" :exclusions [org.clojure/clojure]]
+                 [funcool/clojure.jdbc "0.5.1"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
 
   :javac-options ["-target" "1.7" "-source" "1.7" "-Xlint:-options"]
-  :profiles {:dev {:global-vars {*warn-on-reflection* true
-                                 *unchecked-math* :warn-on-boxed}
-                   :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
+  :profiles {:dev {:global-vars {*warn-on-reflection* true}
+                   :dependencies [[org.clojure/clojure "1.7.0-beta2"]
                                   [postgresql "9.3-1102.jdbc41"]
-                                  [com.h2database/h2 "1.3.176"]
-                                  [cheshire "5.3.1"]]}}
+                                  [com.h2database/h2 "1.4.187"]
+                                  [cheshire "5.4.0"]]}}
   :java-source-paths ["src/java"])
 
