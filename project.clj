798463174eab792de48e42f7b9f6cdb16723cec8
Andrey Antukh <niwi@niwi.nz>
2015-05-20 00:00:06 +0200

    Update jooq version to 3.6.1

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "BSD (2-Clause)"
             :url "http://opensource.org/licenses/BSD-2-Clause"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.jooq/jooq "3.6.0"]
+                 [org.jooq/jooq "3.6.1"]
                  [cats "0.4.0" :exclusions [org.clojure/clojure]]
                  [funcool/clojure.jdbc "0.5.1"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
