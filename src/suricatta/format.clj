a1c5b1fbb5fd10e8277129696b300721f1f5ac06
Andrey Antukh <niwi@niwi.nz>
2015-07-21 22:56:23 +0300

    Add missing email on the license header on format ns.

--- src/suricatta/format.clj
+++ src/suricatta/format.clj
@@ -1,4 +1,4 @@
-;; Copyright (c) 2014, Andrey Antukh
+;; Copyright (c) 2014, Andrey Antukh <niwi@niwi.nz>
 ;; All rights reserved.
 ;;
 ;; Redistribution and use in source and binary forms, with or without
