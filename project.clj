90d59326a590dea3483f2c36820193fd0f7819b1
Andrey Antukh <niwi@niwi.nz>
2015-08-17 22:11:23 +0300

    Set version to 0.4.0-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject funcool/suricatta "0.3.1"
+(defproject funcool/suricatta "0.4.0-SNAPSHOT"
   :description "High level sql toolkit for clojure (backed by jooq library)"
   :url "https://github.com/funcool/suricatta"
   :license {:name "BSD (2-Clause)"
