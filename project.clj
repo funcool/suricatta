4a91fdd5288d994a0c2ca193f5e93d3edef35c86
Andrey Antukh <niwi@niwi.nz>
2015-08-17 21:33:13 +0300

    Update dependencies.

--- project.clj
+++ project.clj
@@ -3,17 +3,18 @@
   :url "https://github.com/funcool/suricatta"
   :license {:name "BSD (2-Clause)"
             :url "http://opensource.org/licenses/BSD-2-Clause"}
-  :dependencies [[org.clojure/clojure "1.6.0" :scope "provided"]
+
+  :dependencies [[org.clojure/clojure "1.7.0" :scope "provided"]
                  [org.jooq/jooq "3.6.2"]
-                 [cats "0.4.0" :exclusions [org.clojure/clojure]]
-                 [funcool/clojure.jdbc "0.5.1"]
+                 [funcool/cats "0.6.0"]
+                 [funcool/clojure.jdbc "0.6.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
 
   :javac-options ["-target" "1.7" "-source" "1.7" "-Xlint:-options"]
   :profiles {:dev {:global-vars {*warn-on-reflection* true}
-                   :dependencies [[org.clojure/clojure "1.7.0-beta2"]
-                                  [postgresql "9.3-1102.jdbc41"]
-                                  [com.h2database/h2 "1.4.187"]
+                   :plugins [[lein-ancient "0.6.7"]]
+                   :dependencies [[postgresql "9.3-1102.jdbc41"]
+                                  [com.h2database/h2 "1.4.188"]
                                   [cheshire "5.5.0"]]}}
   :java-source-paths ["src/java"])
 
