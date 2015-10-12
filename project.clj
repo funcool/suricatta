5ea3f0f51da6b2ef726bfa41c49c6e041eb38635
Andrey Antukh <niwi@niwi.nz>
2015-10-12 11:34:58 +0300

    Set version to 0.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject funcool/suricatta "0.4.0-SNAPSHOT"
+(defproject funcool/suricatta "0.4.0"
   :description "High level sql toolkit for clojure (backed by jooq library)"
   :url "https://github.com/funcool/suricatta"
   :license {:name "BSD (2-Clause)"
@@ -6,12 +6,11 @@
 
   :dependencies [[org.clojure/clojure "1.7.0" :scope "provided"]
                  [org.jooq/jooq "3.7.0"]
-                 [funcool/promissum "0.3.1"]
                  [funcool/cats "1.0.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [funcool/clojure.jdbc "0.6.1"]]
 
-  :javac-options ["-target" "1.7" "-source" "1.7" "-Xlint:-options"]
+  :javac-options ["-target" "1.8" "-source" "1.8" "-Xlint:-options"]
   :profiles {:dev {:global-vars {*warn-on-reflection* true}
                    :plugins [[lein-ancient "0.6.7"]]
                    :dependencies [[postgresql "9.3-1102.jdbc41"]
