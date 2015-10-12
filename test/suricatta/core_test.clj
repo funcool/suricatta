098b154baa37d2f05877379648476ee28d289a50
Andrey Antukh <niwi@niwi.nz>
2015-10-12 11:33:18 +0300

    Fix tests.

--- test/suricatta/core_test.clj
+++ test/suricatta/core_test.clj
@@ -79,9 +79,10 @@
   (testing "Fetch in json format"
     (let [sql "select x, x+1 as i, 'a,b' as k from generate_series(1, 1) as x"
           result (sc/fetch *ctx* sql {:format :json})]
-      (is (= (str "{\"fields\":[{\"name\":\"x\",\"type\":\"INT4\"},"
-                  "{\"name\":\"i\",\"type\":\"INT4\"},"
-                  "{\"name\":\"k\",\"type\":\"OTHER\"}],"
+      (is (= (str "{\"fields\":["
+                  "{\"table\":\"\",\"name\":\"x\",\"type\":\"INT4\"},"
+                  "{\"table\":\"\",\"name\":\"i\",\"type\":\"INT4\"},"
+                  "{\"table\":\"\",\"name\":\"k\",\"type\":\"OTHER\"}],"
                   "\"records\":[[1,2,\"a,b\"]]}")
              result))))
 )
