4400594926b6d3b30a26b9b5de8f313aa0676848
Andrey Antukh <niwi@niwi.nz>
2015-10-17 10:15:21 +0300

    Clear dependencies on project.clj

--- project.clj
+++ project.clj
@@ -5,10 +5,7 @@
             :url "http://opensource.org/licenses/BSD-2-Clause"}
 
   :dependencies [[org.clojure/clojure "1.7.0" :scope "provided"]
-                 [org.jooq/jooq "3.7.0"]
-                 [funcool/cats "1.0.0"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [funcool/clojure.jdbc "0.6.1"]]
+                 [org.jooq/jooq "3.7.0"]]
 
   :javac-options ["-target" "1.8" "-source" "1.8" "-Xlint:-options"]
   :profiles {:dev {:global-vars {*warn-on-reflection* true}
