d8a0ef24b893f2ab995bfaff75fdef6912d3395a
Andrey Antukh <niwi@niwi.nz>
2016-01-06 15:36:21 +0200

    Set version to 0.6.2

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ High level sql toolkit for clojure (backed by jooq library)
 Put suricatta on your dependency list:
 
 ```clojure
-[funcool/suricatta "0.6.0"]
+[funcool/suricatta "0.6.2"]
 [com.h2database/h2 "1.4.190"] ;; For this example only
 ```
 
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject funcool/suricatta "0.6.1"
+(defproject funcool/suricatta "0.6.2"
   :description "High level sql toolkit for clojure (backed by jooq library)"
   :url "https://github.com/funcool/suricatta"
   :license {:name "BSD (2-Clause)"
