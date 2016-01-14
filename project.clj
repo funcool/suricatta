6134b723321e2c4ed2c755838b15e53bc202aa02
Andrey Antukh <niwi@niwi.nz>
2016-01-14 23:54:54 +0200

    Set version to 0.7.0.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject funcool/suricatta "0.6.2"
+(defproject funcool/suricatta "0.7.0"
   :description "High level sql toolkit for clojure (backed by jooq library)"
   :url "https://github.com/funcool/suricatta"
   :license {:name "BSD (2-Clause)"
